2171fcb0d8fd57e21dd5947407b0ef670ca4d07d
captainproloot <162425495+captainproloot@users.noreply.github.com>
2024-04-18 14:01:35 +1200

    Update and rename playnormal.js to play.js

--- commands/play.js
+++ commands/play.js
@@ -1,12 +1,12 @@
 const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
-  name: "playsong",
+  name: "play",
   description: "Play a track.",
   permissions: "0x0000000000000800",
   options: [
     {
-      name: "normal",
+      name: "song",
       description: "Open music from other platforms.",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
